938e27d4a09443b7de10bbcc8f9cbc6546641100
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-23 17:18:06 +0900

    add:ざっくり見た目を整える実装

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <div className="input-form">
+    <div className="inner">
+      <input type="text" className="input" placeholder="TODOを入力してください。" />
+      <button className="btn is-primary">追加</button>
+    </div>
+  </div>
+  <div className="inner">
+    <ul className="task-list">
+      <li>
+        <label className="checkbox-label">
+          <input type="checkbox" className="checkbox-input" />
+        </label>
+        <div><span></span></div>
+        <button className="btn is-delete">削除</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
