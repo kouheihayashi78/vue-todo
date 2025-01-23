dbc9ab072caff5531b267c02b6297bde98d3f229
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-23 18:22:22 +0900

    add:ざっくりデータ表示までを実装

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { TaskList } from '../FoodData.ts'
+
+  const tasks = ref(TaskList)
+  const doubleClick = ref(false)
+
+  const addTask = () => {
+    console.log('追加処理です')
+  }
+  const deleteTask = () => {
+    console.log('削除処理です')
+  }
+
+</script>
 
 <template>
   <div className="input-form">
     <div className="inner">
       <input type="text" className="input" placeholder="TODOを入力してください。" />
-      <button className="btn is-primary">追加</button>
+      <button @click="addTask" className="btn is-primary">追加</button>
     </div>
   </div>
   <div className="inner">
     <ul className="task-list">
-      <li>
+      <li v-for="task in tasks" :key="task.name">
         <label className="checkbox-label">
           <input type="checkbox" className="checkbox-input" />
+          <span v-if="doubleClick === false">{{ task.name }}</span>
         </label>
-        <div><span></span></div>
-        <button className="btn is-delete">削除</button>
+        <button @click="deleteTask" className="btn is-delete">削除</button>
       </li>
     </ul>
   </div>
