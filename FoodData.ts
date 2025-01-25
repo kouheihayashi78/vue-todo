3fe5a840ac8dc919145ac1aa8115068ba0c18263
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-25 12:37:30 +0900

    fix:編集できるように編集状態を保持するデータ追加とcss修正

--- FoodData.ts
+++ FoodData.ts
@@ -1,31 +1,38 @@
 export interface Task {
   name: string,
   isDone: boolean,
+  isEdit: boolean,
 }
 
 export const TaskList:Task[] = [
   {
     name: 'Vue学習',
     isDone: false,
+    isEdit: false,
   },
   {
     name: 'Laravel学習',
     isDone: false,
+    isEdit: false,
   },
   {
     name: '要件定義学習',
     isDone: false,
+    isEdit: false,
   },
   {
     name: 'ジャーナル',
     isDone: true,
+    isEdit: false,
   },
   {
     name: '瞑想',
     isDone: true,
+    isEdit: false,
   },
   {
     name: '書籍学習',
     isDone: true,
+    isEdit: false,
   },
 ];
